c39c0d65bf45a2744d9cf1ce6b0bc6f776674adb
Dhananjay <dhananjay@pop-os.localdomain>
2024-07-26 01:30:46 +0530

    added track add <file-name>

--- trackflow.mjs
+++ trackflow.mjs
@@ -1,5 +1,6 @@
 import path from "path";
 import fs from "fs/promises"
+import crypto from "crypto"
 class Trackflow{
     
     constructor(repoPath = "."){
@@ -19,5 +20,26 @@ class Trackflow{
             console.log("Already initialised the .track folder");
         }
     }
+    hashObject(content){
+        return crypto.createHash('sha1').update(content, 'utf-8').digest('hex');
+    }
+
+    async add(fileToBeAdded){
+        const fileData = await fs.readFile(fileToBeAdded, {encoding: 'utf-8'});
+        const fileHash = this.hashObject(fileData);
+        console.log(fileHash)
+        const hashedObjectPath = path.join(this.objectsPath, fileHash);
+        await fs.writeFile(hashedObjectPath, fileData);
+        await this.updateStagingArea(fileToBeAdded, fileHash)
+        console.log(`Added ${fileToBeAdded}`)
+    }
+
+    async updateStagingArea(filePath, fileHash){
+        const index = JSON.parse(await fs.readFile(this.indexPath, {encoding: 'utf-8'}));
+        index.push({path: filePath, hash: fileHash});
+        await fs.writeFile(this.indexPath, JSON.stringify(index));
+    }
 }
-// const track = new Trackflow();
+
+const track = new Trackflow();
+track.add('sample.txt');
